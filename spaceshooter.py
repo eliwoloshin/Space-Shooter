7dbb24aae3b22c4ef9c561c90064a36305ad4fd5
eliwoloshin <eliwoloshin@hanovernorwichschools.org>
2016-05-16 14:39:50 -0400

    Updated from Brython Server: 5/16/2016 2:39:50 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -55,7 +55,7 @@ class Ship1(SpaceShip):
         
     def __init__(self, app, position, velocity, sun):
         super().__init__(Ship1.asset, app, position, velocity, sun)
-        self.registerKeys(["a", "d", "w", "space"])
+        self.registerKeys(["left arrow", "right arrow", "up arrow", "enter"])
         
     def step(self, T, dT):
         super().step(T, dT)
